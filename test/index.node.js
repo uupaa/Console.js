c76dfa11ccfdf82de26bb8b7b281570914bdec6e
uupaa <uupaa.js@gmail.com>
2014-05-20 04:45:03 +0900

    add Syntax highlight keywords

--- test/index.node.js
+++ test/index.node.js
@@ -1,7 +1,8 @@
 require("../node_modules/uupaa.nodemodule.js/lib/NodeModule.js");
 require("../lib/Console.js");
-require("../node_modules/uupaa.valid.js/lib/Valid.js");
+require("../node_modules/uupaa.help.js/node_modules/uupaa.reflection.js/lib/Reflection.js");
 require("../node_modules/uupaa.help.js/lib/Help.js");
+require("../node_modules/uupaa.valid.js/lib/Valid.js");
 require("../node_modules/uupaa.task.js/lib/Task.js");
 require("../node_modules/uupaa.test.js/lib/Test.js");
 require("../release/Console.min.js");
