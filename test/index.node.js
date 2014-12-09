fb8b35201916cb5a54d5ac2970d8682501aa80d6
uupaa <uupaa.js@gmail.com>
2014-12-09 14:16:40 +0900

    new cli

--- test/index.node.js
+++ test/index.node.js
@@ -1,9 +1,4 @@
-require("../node_modules/uupaa.nodemodule.js/lib/NodeModule.js");
-require("../node_modules/uupaa.reflection.js/lib/Reflection.js");
+require("../node_modules/uupaa.wmdevtools.js/lib/WMDevTools.js");
 require("../lib/Console.js");
-require("../node_modules/uupaa.help.js/lib/Help.js");
-require("../node_modules/uupaa.valid.js/lib/Valid.js");
-require("../node_modules/uupaa.task.js/lib/Task.js");
-require("../node_modules/uupaa.test.js/lib/Test.js");
 require("../release/Console.min.js");
-require("./test.js");
+require("./testcase.js");
